e992c2c5d457d449d4e5a2c72cd7c6e0f276e172
Victor <vitukodb@gmail.com>
2023-10-21 21:18:19 +0100

    Finished exercices

--- module/src/org/ulpgc/is1/model/RepairManager.java
+++ module/src/org/ulpgc/is1/model/RepairManager.java
@@ -1,7 +1,7 @@
 package org.ulpgc.is1.model;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Date;
 
 public class RepairManager {
     private List<Mechanic> mechanics;
@@ -23,8 +23,8 @@ public class RepairManager {
         mechanics.add(mechanic);
     }
 
-    public void addVehicle(String make, String model, Plate plate) {
-        Vehicle vehicle = new Vehicle(make, model, plate);
+    public void addVehicle(String make, String model, Plate plate, Customer customer) {
+        Vehicle vehicle = new Vehicle(make, model, plate, customer);
         vehicles.add(vehicle);
     }
     public void addSparePart(String name, int price){
@@ -73,5 +73,28 @@ public class RepairManager {
 
     public void setRepairs(List<Repair> repairs) {
         this.repairs = repairs;
+    }
+    public void formalizePayment(Repair repair, int paymentAmount) {
+        if (repairs.contains(repair)) {
+            Date paymentDate = new Date();
+            Payment payment = new Payment(paymentDate, paymentAmount);
+
+            repair.getPayments().add(payment);
+
+            System.out.println("Pago formalizado por: " + repair.getId() + ". Precio: " + paymentAmount + "€");
+        } else {
+            System.out.println("No se encontró el pago");
+        }
+    }
+    public void deleteVehicle(Vehicle vehicle) {
+        if (vehicles.contains(vehicle)) {
+            vehicles.remove(vehicle);
+            System.out.println("Vehículo eliminado: " + vehicle.getMake() + " " + vehicle.getModel());
+        } else {
+            System.out.println("No se encontró vehículo para eliminar.");
+        }
+    }
+    public int countVehicles() {
+        return vehicles.size();
     }
 }
--- module/src/org/ulpgc/is1/model/Vehicle.java
+++ module/src/org/ulpgc/is1/model/Vehicle.java
@@ -4,11 +4,13 @@ public class Vehicle {
     String make;
     String model;
     Plate plate;
+    Customer customer;
 
-    public Vehicle(String make, String model, Plate plate) {
+    public Vehicle(String make, String model, Plate plate, Customer customer) {
         this.make = make;
         this.model = model;
         this.plate = plate;
+        this.customer = customer;
     }
 
     public String getMake() {
@@ -21,5 +23,25 @@ public class Vehicle {
 
     public Plate getPlate() {
         return plate;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPlate(Plate plate) {
+        this.plate = plate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 }
