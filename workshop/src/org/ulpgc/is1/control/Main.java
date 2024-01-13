0c5739e160c3fd5c8b5b675c883f0f7dc5f4e095
Victor <vitukodb@gmail.com>
2024-01-13 22:44:18 +0000

    Finished code

--- workshop/src/org/ulpgc/is1/control/Main.java
+++ workshop/src/org/ulpgc/is1/control/Main.java
@@ -2,6 +2,7 @@ package org.ulpgc.is1.control;
 
 import org.ulpgc.is1.model.*;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -14,7 +15,7 @@ public class Main {
         Plate plate1 = new Plate("2043FDL");
         Vehicle vehicle1 = new Vehicle("Toyota", "Camry", plate1, customer1);
 
-        Phone customerPhone2 = new Phone("678991123");
+        Phone customerPhone2 = new Phone("678991125");
         Customer customer2 = new Customer("Laura", customerPhone2);
         Plate plate2 = new Plate("5512GBT");
         Vehicle vehicle2 = new Vehicle("Mercedes", "Benz", plate2, customer2);
@@ -37,27 +38,29 @@ public class Main {
         System.out.println(repairManager.getMechanic(0).getName() + " " + repairManager.getMechanic(0).getSurname());
 
         System.out.println("------------Datos del segundo vehículo------------");
-        System.out.println(repairManager.getVehicle(1).getMake() + " " + repairManager.getVehicle(1).getModel());
-        System.out.println(repairManager.getVehicle(1).getPlate().getPlate());
-        System.out.println(repairManager.getVehicle(1).getCustomer().getName());
+        System.out.println("Vehículo: " + repairManager.getVehicle(1).getMake() + " " + repairManager.getVehicle(1).getModel());
+        System.out.println("Matrícula: " + repairManager.getVehicle(1).getPlate().getPlate());
+        System.out.println("Propietario: " + repairManager.getVehicle(1).getCustomer().getName());
 
         System.out.println("------------Registro de reparación------------");
         Mechanic mechanic = repairManager.getMechanic(0);
         Vehicle vehicle = repairManager.getVehicle(1);
         SparePart[] spareParts = {repairManager.getSparePart(0), repairManager.getSparePart(1)};
         repairManager.registerRepair(mechanic, vehicle, spareParts, BreakdownTypes.mechanical);
+        System.out.println("Mecánico: " + mechanic.getName() + " " + mechanic.getSurname());
+        System.out.println("Vehículo reparado: " + vehicle.getMake() + " " + vehicle.getModel());
+        System.out.println("Piezas modificadas: " + spareParts[0].getName() + " and " + spareParts[1].getName());
 
-        System.out.println("------------Formalizar el pago------------");
+        System.out.println("------------Pago formalizado------------");
         Repair repair = vehicle.getRepair(0);
         Date paymentDate = new Date();
         Payment payment = new Payment(paymentDate, repair.price());
         repair.addPayment(payment);
         System.out.println("Fecha de pago: " + payment.getDate());
-        System.out.println("Monto pagado: " + payment.getAmount());
+        System.out.println("Monto pagado: " + payment.getAmount() + "€");
 
         System.out.println("------------Borrar el primer vehículo------------");
         repairManager.removeVehicle(0);
-
         System.out.println("Nº de vehículos: " + repairManager.getVehicleList().size());
 
         System.out.println("------------Información de la reparación------------");
@@ -67,6 +70,6 @@ public class Main {
         for (Item item : repair.getItems()) {
             System.out.println(item.getQuantity() + "x " + item.getSparePart().getName());
         }
-        System.out.println("Total a pagar: " + repair.price());
+        System.out.println("Total a pagar: " + repair.price() + "€");
     }
 }
